919d83b6a3a6e50cd23a81d46d5fe7cfb847d438
gaolingxiao <ygaolingxiaosensetime.com>
2025-02-11 15:46:21 +0800

    feat: add flash attn operator

--- src/kernel/develop_flash_attn/develop_flash_attn.md
+++ src/kernel/develop_flash_attn/develop_flash_attn.md
@@ -36,4 +36,43 @@ p_i = p_i * l_{pre} / l_{cur}
 $$
 这样就使局部归一化值更新为全局归一化值。
 
-代码在文件[tile_softmax.py](./tile_softmax.py)中：
+代码在文件[tile_softmax.py](./tile_softmax.py)中
+
+### 3.使用torch实现的类flash attn v2代码
+
+其中局部softmax按照第二节所述，在softmax以外，我们需要将q, k, v三个矩阵分成多个tile(block * block)。对seq_len进行切分，首先循环q_tile，再循环k_tile和v_tile，最终计算出当前当前block的attn_score。
+
+attn v2官方代码如下所述![flash_attn_v2算法流程图](./images/flash_attn_v2.png)，这个的主要难点在于局部softmax。
+
+本块代码文件在[flash_attn_mock_code.py](./flash_attn_mock_code.py)
+
+### 4.使用triton编写真正的flash attn v2代码
+
+根据之前的思想，使用triton的接口实现起来，直接上代码：[flash_attn_v2](./flash_attn_v2.py)
+
+
+### 5.性能优化
+
+通过ncu收集算子性能数据, 使用ncu记录数据的时候，最好把输出语句注释掉，否则会记录很多额外的算子，可能是张量太大，输出的时候要从显存copy到主存（我这里记录了100多个==PROF== Profiling "CatArrayBatchedCopy" - 89: 0%....50%....100% - 31 passes）
+```
+ncu --target-processes all --set full -o flash_attn_test python flash_attn_v2.py
+```
+我分别收集了BLOCK=16, 31, 64的flash_attn算子性能数据，发现BLOCK的大小对flash_attn算子的性能影响较大, BLOCK=16的推理时间最长，其次是BLOCK32，最优是BLOCK64，当BLOCK=128是，GPU提示SM资源不足。
+
+**BLOCK=64的性能数据**
+![BLOCK=64](./images/block64.png)
+**BLOCK=32的性能数据**
+![BLOCK=32](./images/block32.png)
+
+所以为了使算子有最佳性能，要使BLOCK = math.floor(SM_SHM_SIZE // element_type_size // HEAD_DIM), 因为找不到接口直接获取SM_SHM_SIZE， 于是使用triton.autotune接口，测试哪一种BLOCK大小推理速度更快。
+```
+configs = [
+    triton.Config({'BLOCK_M': BM, 'BLOCK_N': BN}, num_stages=s, num_warps=w) \
+    for BM, BN in [(32, 32), (64, 64)]\
+    for s in [1, 2, 3]\
+    for w in [1, 2, 4, 8]\
+]
+
+@triton.autotune( configs, key=["HEAD_DIM"])
+```
+加了autotune后，发现会对attn_fwd算子收集比预想中多得多的数据，原因是autotune对一个配置会重复执行多次算子，于是收集到的数据就会非常多。
--- src/kernel/develop_flash_attn/flash_attn_mock_code.py
+++ src/kernel/develop_flash_attn/flash_attn_mock_code.py
@@ -2,61 +2,62 @@ import torch
 
 def MhaAttn(q, k, v):
     qk = q @ k.transpose(-2, -1)
-    qk = qk / (qk.size(-1) ** 2)
+    qk = qk / (q.size(-1) ** 0.5)
+    qk = torch.softmax(qk, dim=-1)
     attn_score = qk @ v
     return attn_score
 
 # 假设SRAM大小为4096
 SRAM_SIZE = 4096
-# q = [b, h, seq_len, d_k]
-# 该算子以fp16进行计算
+
 def flash_attn_v2(q, k, v):
-    BLOCK = SRAM_SIZE // q.size(-1)
-    
+    BLOCK = SRAM_SIZE // q.size(-1) // 4
     seq_len = q.size(-2)
     batch_size = q.size(0)
     head_num = q.size(1)
     head_dim = q.size(-1)
-    M = torch.empty(batch_size, head_num, BLOCK, 1).fill_(float('-inf'))
-    L = torch.empty(batch_size, head_num, BLOCK, head_dim)
-    O = torch.empty(batch_size, head_num, seq_len, head_dim)
+    M = -torch.inf * torch.ones(batch_size, head_num, seq_len, 1)
+    L = torch.zeros(batch_size, head_num, seq_len, head_dim)
+    O = torch.zeros(batch_size, head_num, seq_len, head_dim)
     
     for i in range(0, seq_len, BLOCK):
         q_block = q[:, :, i:i+BLOCK, :]
         o_block = O[:, :, i:i+BLOCK, :]
+        m_block = M[:, :, i:i+BLOCK, :]
+        l_block = L[:, :, i:i+BLOCK, :]
         
         for j in range(0, seq_len, BLOCK):
             k_block = k[:, :, j:j+BLOCK, :]
             v_block = v[:, :, j:j+BLOCK, :]
+            qk_block = q_block @ k_block.transpose(-2, -1) / (head_dim ** 0.5)
+            current_m = torch.max(qk_block, dim=-1, keepdim=True).values
+            M_new = torch.maximum(m_block, current_m)
             
-            qk_block = torch.matmul(q_block, k_block.transpose(-2, -1))
-            qk_block = qk_block / (head_dim ** 0.5)
-            
-            M_new = torch.max(M, torch.max(qk_block, dim=-1))
-            L = L * torch.exp(M - M_new) + torch.sum(torch.exp(qk_block - M_new))
-            o_block = torch.exp(qk_block - M_new) / L + o_block * torch.exp(M - M_new)
-            
-            M = M_new
-            
+            l_block = l_block * torch.exp(m_block - M_new) + torch.sum(torch.exp(qk_block - M_new), dim = -1, keepdim=True)
+            o_block = o_block * torch.exp(m_block - M_new) + torch.exp(qk_block - M_new) @ v_block
+            m_block = M_new
+        o_block = o_block / l_block
+        O[:, :, i:i+BLOCK, :] = o_block
+
     return O
             
-
 def test_flash_v2():
-    N, d = 3, 128  # 更新N和d的值
-
-    Q_mat = torch.rand((N, d))
-    K_mat = torch.rand((N, d))
-    V_mat = torch.rand((N, d))
+    N, d = 256, 128  # 更新N和d的值
+    B, h = 2, 16
+    Q_mat = torch.rand((B, h, N, d), dtype=torch.float32)
+    K_mat = torch.rand((B, h, N, d), dtype=torch.float32)
+    V_mat = torch.rand((B, h, N, d), dtype=torch.float32)
 
     # 执行flash attention计算
     flash_attention_v2_output = flash_attn_v2(Q_mat, K_mat, V_mat)
 
     # 执行标准的PyTorch softmax和attention计算
-    _, expected_attention = MhaAttn(Q_mat, K_mat, V_mat)
+    expected_attention = MhaAttn(Q_mat, K_mat, V_mat)
     print(flash_attention_v2_output)
     print(expected_attention)
+    print(f"output diff: {expected_attention - flash_attention_v2_output}")
     # 断言flash attention计算的结果与标准计算结果是否接近
-    assert torch.allclose(flash_attention_v2_output, expected_attention), "Error in flash attention calculation"
+    assert torch.allclose(flash_attention_v2_output, expected_attention, atol=1e-07), "Error in flash attention calculation"
 
 if __name__ == '__main__':
     test_flash_v2()
